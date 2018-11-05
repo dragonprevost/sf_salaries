cf19d3da8b06bd42ec17815f5141c74037990e4e
dragonprevost <dragon@dkp.io>
2018-11-04 18:47:55 -0800

    csvReader is printing values in csv

---
+++ csvReader.py
@@ -0,0 +1,16 @@
+import csv
+
+
+class DataReader:
+    def __init__(self):
+        self.csv_path = 'data/executives_search_list.csv'
+        self.csv_file = open(self.csv_path, newline='')
+
+    def csv_to_list(self):
+        self.csv_reader = csv.reader(self.csv_file, delimiter=',')
+        for item in self.csv_reader: 
+            print(item)
+
+
+dr = DataReader()
+dr.csv_to_list()
