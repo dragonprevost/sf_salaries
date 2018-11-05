47fcd70a91de5f76d8c06b7fccf3faab9a539255
dragonprevost <dragon@dkp.io>
2018-11-04 19:10:41 -0800

    changed csv path to salaries csv

--- csvReader.py
+++ csvReader.py
@@ -3,13 +3,16 @@ import csv
 
 class DataReader:
     def __init__(self):
-        self.csv_path = 'data/executives_search_list.csv'
+        self.csv_path = 'data/Salaries.csv'
         self.csv_file = open(self.csv_path, newline='')
 
     def csv_to_list(self):
         self.csv_reader = csv.reader(self.csv_file, delimiter=',')
         for item in self.csv_reader: 
             print(item)
+
+
+
 
 
 dr = DataReader()
